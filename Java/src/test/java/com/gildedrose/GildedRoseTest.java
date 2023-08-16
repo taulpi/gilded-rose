7fb7e0329050913eb8ce1722e3da5a778da19cee
Paul Tietz <pati@aprixon.de>
2023-08-16 13:26:29 +0200

    cover logic with tests: parameterized tests for sellin decreased and quality decreased

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,20 +1,44 @@
 package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void theSellinIsIncreased() {
-        Item item = new Item("Some non special Item", 0, 0);
+    @ParameterizedTest
+    @CsvSource({
+        "0, -1",
+        "2, 1",
+        "1, 0",
+        "999, 998",
+        "-1, -2",
+        "-111, -112",
+    })
+    void theSellinIsDecreased(int sellIn, int expected) {
+        Item item = new Item("some Item", sellIn, 0);
 
         whenQualityIsUpdated(item);
 
-        assertEquals(-1, item.sellIn);
-
+        assertEquals(expected, item.sellIn);
     }
+
+    @ParameterizedTest
+    @CsvSource({
+        "2, 1",
+        "1, 0",
+        "999, 998",
+    })
+    void qualityIsDecreased(int quality, int expected) {
+        Item item = new Item("some Item", 5, quality);
+
+        whenQualityIsUpdated(item);
+
+        assertEquals(expected, item.quality);
+    }
+
     @Test
     void qualityDoesNotDecreaseBelow0() {
         Item item = new Item("Some non special Item", 0, 0);
