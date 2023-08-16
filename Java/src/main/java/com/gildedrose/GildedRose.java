ea1154f518a8bef49d8eb0e10f63aea447f816b3
Paul Tietz <pati@aprixon.de>
2023-08-16 21:53:24 +0200

    refactor: cleanup nested classes and extract UpdatedBackstageItem with duplicate code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -17,11 +17,15 @@ class GildedRose {
 
     private UpdatedItem updated(Item item) {
         if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return new UpdatedOrdinaryItem.UpdatedSulfuras(item);
+            return new UpdatedSulfuras(item);
         }
         if (item.name.equals("Aged Brie")) {
-            return new UpdatedOrdinaryItem.UpdatedAgedBrie(item);
+            return new UpdatedAgedBrie(item);
         }
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return new UpdatedBackstageItem(item);
+        }
+
         return new UpdatedOrdinaryItem(item);
     }
 
@@ -77,34 +81,6 @@ class GildedRose {
             return updatedQuality;
         }
 
-        private static class UpdatedAgedBrie implements UpdatedItem {
-            private Item item;
-
-            public UpdatedAgedBrie(Item item) {
-                this.item = item;
-            }
-
-            @Override
-            public int sellIn() {
-                return item.sellIn - 1;
-            }
-
-            @Override
-            public int quality() {
-                int updatedQuality = item.quality;
-                if (updatedQuality < 50) {
-                    updatedQuality++;
-                }
-
-
-                if (item.sellIn < 1) {
-                    if (updatedQuality < 50) {
-                        updatedQuality++;
-                    }
-                }
-                return updatedQuality;
-            }
-        }
 
         private static class UpdatedSulfuras implements UpdatedItem {
             private Item item;
@@ -126,6 +102,112 @@ class GildedRose {
                 }
                 return updatedQuality;
             }
+        }
+
+
+    }
+
+    private static class UpdatedBackstageItem implements UpdatedItem {
+        private Item item;
+
+        public UpdatedBackstageItem(Item item) {
+            this.item = item;
+        }
+
+        @Override
+        public int sellIn() {
+            return item.sellIn - 1;
+        }
+
+        @Override
+        public int quality() {
+            int updatedQuality = item.quality;
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    updatedQuality--;
+                }
+            } else {
+                if (updatedQuality < 50) {
+                    updatedQuality++;
+
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.sellIn < 11) {
+                            if (updatedQuality < 50) {
+                                updatedQuality++;
+                            }
+                        }
+
+                        if (item.sellIn < 6) {
+                            if (updatedQuality < 50) {
+                                updatedQuality++;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            if (item.sellIn < 1) {
+                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (updatedQuality > 0) {
+                        updatedQuality--;
+                    }
+                } else {
+                    updatedQuality -= updatedQuality;
+                }
+            }
+            return updatedQuality;
+        }
+    }
+
+    private static class UpdatedAgedBrie implements UpdatedItem {
+        private Item item;
+
+        public UpdatedAgedBrie(Item item) {
+            this.item = item;
+        }
+
+        @Override
+        public int sellIn() {
+            return item.sellIn - 1;
+        }
+
+        @Override
+        public int quality() {
+            int updatedQuality = item.quality;
+            if (updatedQuality < 50) {
+                updatedQuality++;
+            }
+
+
+            if (item.sellIn < 1) {
+                if (updatedQuality < 50) {
+                    updatedQuality++;
+                }
+            }
+            return updatedQuality;
+        }
+    }
+
+    private static class UpdatedSulfuras implements UpdatedItem {
+        private Item item;
+
+        public UpdatedSulfuras(Item item) {
+            this.item = item;
+        }
+
+        @Override
+        public int sellIn() {
+            return item.sellIn;
+        }
+
+        @Override
+        public int quality() {
+            int updatedQuality = item.quality;
+            if (updatedQuality < 50) {
+                updatedQuality++;
+            }
+            return updatedQuality;
         }
     }
 }
