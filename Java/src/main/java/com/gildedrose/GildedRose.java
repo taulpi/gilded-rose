f19fd38bc3556f29015c25f2dc07db7b132d60c2
Paul Tietz <pati@aprixon.de>
2023-08-16 21:31:44 +0200

    refactor: extract Method Object

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,66 +9,75 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            item.quality = updatedQuality(item);
-            item.sellIn = updatedSellIn(item);
+            UpdatedItem updatedItem = new UpdatedItem(item);
+            item.quality = updatedItem.quality();
+            item.sellIn = updatedItem.sellIn();
         }
     }
 
-    private int updatedSellIn(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return item.sellIn;
-        } else {
-            return item.sellIn - 1;
+    private class UpdatedItem {
+        private Item item;
+
+        public UpdatedItem(Item item) {
+            this.item = item;
         }
-    }
 
-    private int updatedQuality(Item item) {
-        int updatedQuality = item.quality;
-        if (!item.name.equals("Aged Brie")
-            && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    updatedQuality--;
-                }
-            }
-        } else {
-            if (updatedQuality < 50) {
-                updatedQuality++;
-
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (updatedQuality < 50) {
-                            updatedQuality++;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (updatedQuality < 50) {
-                            updatedQuality++;
-                        }
-                    }
-                }
+        private int sellIn() {
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                return item.sellIn;
+            } else {
+                return item.sellIn - 1;
             }
         }
 
-
-        if (item.sellIn < 1) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (updatedQuality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            updatedQuality--;
-                        }
+        public int quality() {
+            int updatedQuality = item.quality;
+            if (!item.name.equals("Aged Brie")
+                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        updatedQuality--;
                     }
-                } else {
-                    updatedQuality -= updatedQuality;
                 }
             } else {
                 if (updatedQuality < 50) {
                     updatedQuality++;
+
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.sellIn < 11) {
+                            if (updatedQuality < 50) {
+                                updatedQuality++;
+                            }
+                        }
+
+                        if (item.sellIn < 6) {
+                            if (updatedQuality < 50) {
+                                updatedQuality++;
+                            }
+                        }
+                    }
                 }
             }
+
+
+            if (item.sellIn < 1) {
+                if (!item.name.equals("Aged Brie")) {
+                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (updatedQuality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                updatedQuality--;
+                            }
+                        }
+                    } else {
+                        updatedQuality -= updatedQuality;
+                    }
+                } else {
+                    if (updatedQuality < 50) {
+                        updatedQuality++;
+                    }
+                }
+            }
+            return updatedQuality;
         }
-        return updatedQuality;
     }
 }
