2a1f579db35369db9699353a0d3ace2673ffea25
Paul Tietz <pati@aprixon.de>
2023-08-16 21:44:45 +0200

    refactor: extract UpdatedAgedBrie with duplicate code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -19,6 +19,9 @@ class GildedRose {
         if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             return new OrdinaryUpdatedItem.UpdatedSulfuras(item);
         }
+        if (!item.name.equals("Aged Brie")) {
+            return new OrdinaryUpdatedItem.UpdatedAgedBrie(item);
+        }
         return new OrdinaryUpdatedItem(item);
     }
 
@@ -79,6 +82,88 @@ class GildedRose {
                 }
             }
             return updatedQuality;
+        }
+
+        private static class UpdatedAgedBrie implements UpdatedItem {
+            private Item item;
+
+            public UpdatedAgedBrie(Item item) {
+                this.item = item;
+            }
+
+            @Override
+            public int sellIn() {
+                return item.sellIn - 1;
+            }
+
+            @Override
+            public int quality() {
+                int updatedQuality = item.quality;
+                if (!item.name.equals("Aged Brie")
+                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality > 0) {
+                        updatedQuality--;
+                    }
+                } else {
+                    if (updatedQuality < 50) {
+                        updatedQuality++;
+
+                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (item.sellIn < 11) {
+                                if (updatedQuality < 50) {
+                                    updatedQuality++;
+                                }
+                            }
+
+                            if (item.sellIn < 6) {
+                                if (updatedQuality < 50) {
+                                    updatedQuality++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+                if (item.sellIn < 1) {
+                    if (!item.name.equals("Aged Brie")) {
+                        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (updatedQuality > 0) {
+                                updatedQuality--;
+                            }
+                        } else {
+                            updatedQuality -= updatedQuality;
+                        }
+                    } else {
+                        if (updatedQuality < 50) {
+                            updatedQuality++;
+                        }
+                    }
+                }
+                return updatedQuality;
+            }
+
+            private static class UpdatedSulfuras implements UpdatedItem {
+                private Item item;
+
+                public UpdatedSulfuras(Item item) {
+                    this.item = item;
+                }
+
+                @Override
+                public int sellIn() {
+                    return item.sellIn;
+                }
+
+                @Override
+                public int quality() {
+                    int updatedQuality = item.quality;
+                    if (updatedQuality < 50) {
+                        updatedQuality++;
+                    }
+                    return updatedQuality;
+                }
+            }
         }
 
         private static class UpdatedSulfuras implements UpdatedItem {
